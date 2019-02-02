91ab03056da7b19f5b5a2d3e8ed7723f5928d924
kings <963987632@qq.com>
2019-02-02 19:29:43 +0800

    modify: 异步任务完善

--- king-boot-basic/src/main/java/com/kingboot/basic/task/AsyncTask.java
+++ king-boot-basic/src/main/java/com/kingboot/basic/task/AsyncTask.java
@@ -2,7 +2,10 @@ package com.kingboot.basic.task;
 
 
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.Future;
 
 /**
  * <p class="detail">
@@ -17,25 +20,28 @@ import org.springframework.stereotype.Component;
 @Async
 public class AsyncTask {
     
-    public void task1() throws InterruptedException {
+    public Future<String> task1() throws InterruptedException {
         long begin = System.currentTimeMillis();
         Thread.sleep(1000);
         long end = System.currentTimeMillis();
         System.out.println("任务1耗时:"+(end-begin));
+        return new AsyncResult<>("任务1");
     }
     
-    public void task2() throws InterruptedException {
+    public Future<String> task2() throws InterruptedException {
         long begin = System.currentTimeMillis();
         Thread.sleep(2000);
         long end = System.currentTimeMillis();
         System.out.println("任务2耗时:"+(end-begin));
+        return new AsyncResult<>("任务2");
     }
     
-    public void task3() throws InterruptedException {
+    public Future<String> task3() throws InterruptedException {
         long begin = System.currentTimeMillis();
         Thread.sleep(3000);
         long end = System.currentTimeMillis();
         System.out.println("任务3耗时:"+(end-begin));
+        return new AsyncResult<>("任务3");
     }
 }
  
