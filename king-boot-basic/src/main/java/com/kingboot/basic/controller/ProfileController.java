6f2d74c88a5b56798133b1851f59872d6c283972
kings <963987632@qq.com>
2019-02-04 00:02:30 +0800

    modify: logback整理

--- king-boot-basic/src/main/java/com/kingboot/basic/controller/ProfileController.java
+++ king-boot-basic/src/main/java/com/kingboot/basic/controller/ProfileController.java
@@ -1,15 +1,17 @@
 package com.kingboot.basic.controller;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping ("/profile")
 @RestController
 public class ProfileController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProfileController.class);
     
     @Value ("${test.version}")
     private String version;
