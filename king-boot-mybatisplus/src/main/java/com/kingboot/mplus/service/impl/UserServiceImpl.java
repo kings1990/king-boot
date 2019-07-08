490830ef36cc050f379793c4020d3ca64622a49d
kings <963987632@qq.com>
2019-07-08 15:01:01 +0800

    add:代码生成优化

--- king-boot-mybatisplus/src/main/java/com/kingboot/mplus/service/impl/UserServiceImpl.java
+++ king-boot-mybatisplus/src/main/java/com/kingboot/mplus/service/impl/UserServiceImpl.java
@@ -7,12 +7,7 @@ import com.kingboot.mplus.service.UserService;
 import org.springframework.stereotype.Service;
 
 
-/**
- * 用户
- *
- * @author Kings
- * @date 2019-07-08 14:24:50
- */
+
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
 
