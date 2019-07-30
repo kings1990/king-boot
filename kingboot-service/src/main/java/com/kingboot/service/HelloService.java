712281073cd6178832b2113044b65bbc74d4e18b
kings <963987632@qq.com>
2019-07-30 10:45:23 +0800

    modify：tidy kingboot-service via alibaba code style

--- kingboot-service/src/main/java/com/kingboot/service/HelloService.java
+++ kingboot-service/src/main/java/com/kingboot/service/HelloService.java
@@ -1,11 +1,44 @@
 package com.kingboot.service;
 
 
+/**
+ * <p class="detail">
+ * 功能:hello测试接口
+ * </p>
+ * @author Kings
+ * @ClassName HelloService
+ * @Version V1.0.
+ * @date 2019.07.30 10:44:08
+ */
 public interface HelloService {
+	/**
+	 * <p class="detail">
+	 * 功能:测试
+	 * </p>
+	 * @return string
+	 * @author Kings
+	 * @date 2019.07.30 10:44:08
+	 */
 	String hello();
 	
+	/**
+	 * <p class="detail">
+	 * 功能:测试
+	 * </p>
+	 * @return string
+	 * @author Kings
+	 * @date 2019.07.30 10:44:08
+	 */
 	String hi();
 	
+	/**
+	 * <p class="detail">
+	 * 功能:测试
+	 * </p>
+	 * @return string
+	 * @author Kings
+	 * @date 2019.07.30 10:44:08
+	 */
 	String hehe();
 	
 }
